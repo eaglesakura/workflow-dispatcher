2cce1fe0f71401da91395353bb70ec96057f090f
eaglesakura <eagle.sakura@gmail.com>
2021-02-18 16:19:53 +0900

    add: 'ActivityResult.result' extension for compatible to old version refs #12

--- workflow-dispatcher-runtime/src/main/java/com/eaglesakura/workflowdispatcher/activity/ActivityResultExtensions.kt
+++ workflow-dispatcher-runtime/src/main/java/com/eaglesakura/workflowdispatcher/activity/ActivityResultExtensions.kt
@@ -4,13 +4,29 @@ import android.app.Activity.RESULT_OK
 import android.content.Intent
 import androidx.activity.result.ActivityResult
 
-val ActivityResult.ok
+/**
+ * result is 'RESULT_OK'.
+ */
+val ActivityResult.ok: Boolean
     get() = this.resultCode == RESULT_OK
+
+@Deprecated("replace to 'resultCode'", ReplaceWith("resultCode"))
+val ActivityResult.result: Int
+    get() = this.resultCode
 
 /**
  * returns non-null intent.
  */
+@Deprecated("replace to 'requireData'", ReplaceWith("requireData"))
 fun ActivityResult.requireIntent(): Intent =
+    requireNotNull(data) {
+        "data is null, result='$resultCode'"
+    }
+
+/**
+ * returns non-null intent.
+ */
+fun ActivityResult.requireData(): Intent =
     requireNotNull(data) {
         "data is null, result='$resultCode'"
     }
